203a872167525e4b4f7088c96a8f3c70ec6c85a6
S0haibZafar <sohaibzafar71@gmail.com>
2023-04-16 13:27:08 +0500

    Link new images

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -12,6 +12,10 @@ export class FoodService {
       './assets/food5.jpg',
       './assets/food3.jpg',
       './assets/food2.jpg',
+      './assets/food10.jpg',
+      './assets/food11.jpg',
+      './assets/food12.jpg',
+      './assets/food13.jpg',
       './assets/food1.jpg',
       './assets/food4.jpg',
     ]
