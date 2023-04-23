519b8990ec9d7084b057a5847b64c86ea986aadc
S0haibZafar <sohaibzafar71@gmail.com>
2023-04-23 19:27:51 +0500

    Creat Serach component to search food item

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  searchItem:string = '';
+  constructor(private route : ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      if(params['searchItem'])
+      this.searchItem = params['searchItem'];
+    });
+  }
+
+}
