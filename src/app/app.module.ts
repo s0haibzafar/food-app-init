76c573bdfaf1044208944d27e97ed237910c5a9c
Sohaib-Zafar <100518647+S0haibZafar@users.noreply.github.com>
2023-04-23 18:29:49 +0500

    Cart section (#2)
    * Create model for food
    
    * Import model and set image url by model
    
    * Add food data by using model
    
    * Import rating module
    
    * Add star-rating UI under food items
    
    * Show origin means show tags under food-item
    
    * Set width to show origin properly
    
    * Add cookTime and price
    
    * set origin variable
    
    * Change rating
    
    * Add ng-starrating to show rating on product item
    
    * Add styling for product item card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import {RatingModule} from 'ng-starrating';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RatingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Foods } from 'src/app/shared/models/food';
 
 @Injectable({
   providedIn: 'root'
@@ -7,17 +8,38 @@ export class FoodService {
 
   constructor() { }
 
-  getAll():string[]{
+  getAll():Foods[]{
     return[
-      './assets/food5.jpg',
-      './assets/food3.jpg',
-      './assets/food2.jpg',
-      './assets/food10.jpg',
-      './assets/food11.jpg',
-      './assets/food12.jpg',
-      './assets/food13.jpg',
-      './assets/food1.jpg',
-      './assets/food4.jpg',
+      {
+        id:1,
+        name:'Pizza Pepperoni',
+        cookTime:'10-20',
+        price: 10,
+        favorite: false,
+        origin: ['italy'],
+        star:4.0,
+        imageUrl:'./assets/food5.jpg',
+        tags:['FastFood','Pizza','Lunch']
+      },
+      {
+        id:2,
+        name:'Meatball',
+        cookTime:'20-30',
+        price: 20,
+        favorite: true,
+        origin: ['persian', 'middle east', 'china'],
+        star:4.5,
+        imageUrl:'./assets/food3.jpg',
+        tags:['SlowFood','Lunch']
+      }
+
+      // './assets/food2.jpg',
+      // './assets/food10.jpg',
+      // './assets/food11.jpg',
+      // './assets/food12.jpg',
+      // './assets/food13.jpg',
+      // './assets/food1.jpg',
+      // './assets/food4.jpg',
     ]
   }
 }
