36b0bcc603003821eae60dfe9a9c238d22d03181
S0haibZafar <sohaibzafar71@gmail.com>
2023-04-24 13:08:00 +0500

    Create tag model and tag compoent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import {RatingModule} from 'ng-starrating';
 import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SearchComponent
+    SearchComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tags/tags.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Tag } from '../shared/models/tag';
+import { FoodService } from '../services/food/food.service';
+
+@Component({
+  selector: 'app-tags',
+  templateUrl: './tags.component.html',
+  styleUrls: ['./tags.component.css']
+})
+export class TagsComponent implements OnInit {
+
+  tagData: Tag[] = [];
+
+  constructor(private fs : FoodService) { }
+
+  ngOnInit(): void {
+    this.tagData = this.fs.getAllTags();
+  }
+
+}
