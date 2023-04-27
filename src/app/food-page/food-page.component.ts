8d0f06c248eecb92aa6c091f1dfc5848e2bf0032
S0haibZafar <sohaibzafar71@gmail.com>
2023-04-27 15:05:21 +0500

    Show data of food item by id

--- src/app/food-page/food-page.component.ts
+++ src/app/food-page/food-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Foods } from '../shared/models/food';
+import { ActivatedRoute } from '@angular/router';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-food-page',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoodPageComponent implements OnInit {
 
-  constructor() { }
+  food!:Foods;
+  constructor(private route: ActivatedRoute, private fs: FoodService) {
+   
+   }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params)=>{
+      if(params['id']){
+        this.food = this.fs.getFoodById(params['id']);
+      }
+    });
   }
 
 }
