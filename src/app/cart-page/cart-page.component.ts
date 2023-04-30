8f2f302384f2d97de8371415b7543225cd29fd6d
S0haibZafar <sohaibzafar71@gmail.com>
2023-04-30 18:36:25 +0500

    Import cartItem service and getCart, removeFromCart, chgQuantity

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from '../shared/models/cart';
+import { CartService } from '../services/cart/cart.service';
+import { CartItem } from '../shared/models/cartItem';
 
 @Component({
   selector: 'app-cart-page',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPageComponent implements OnInit {
 
-  constructor() { }
+  cart!:Cart;
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+  }
+
+  setCart(){
+    this.cart = this.cartService.getCart();
+  }
+
+  removeFromCart(cartItem: CartItem){
+    this.cartService.removeFromCart(cartItem.food.id);
+    this.setCart();
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString:string){
+    const quantity = parseInt(quantityInString); 
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+    this.setCart();
   }
 
 }
