f8bf587508a2ac85b2440b150d3f4ab35020ae9b
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-03 09:18:47 +0500

    Remove dummy food items from cart list

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Cart } from '../shared/models/cart';
 import { CartService } from '../services/cart/cart.service';
 import { CartItem } from '../shared/models/cartItem';
-import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-cart-page',
@@ -13,11 +12,7 @@ export class CartPageComponent implements OnInit {
 
   cart!:Cart;
 
-  constructor(private cartService: CartService, private fs: FoodService) {
-    let food = this.fs.getAll();
-    this.cartService.addToCart(food[1]);
-    this.cartService.addToCart(food[0]);
-
+  constructor(private cartService: CartService) {
     this.setCart();
    }
 
